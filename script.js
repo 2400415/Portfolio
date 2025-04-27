e02435b86deca90987b0d5883160e8c274f151b5
Regoemetse Selemala <162500595+2400415@users.noreply.github.com>
2025-04-27 22:19:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const  name=document.querySelector('#heading');
+
+name.addEventListener('click',function(){
+
+  name.textContent="Completed !! ";
+
+});
+
+// testing if the file is working
+
+console.log("Test ok ");
+
+/*
+
+* */
